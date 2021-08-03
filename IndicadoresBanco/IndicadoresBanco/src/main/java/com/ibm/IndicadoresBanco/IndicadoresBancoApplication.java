7c8f031c7523f61c3241e6ef009de146265024e3
Janeth Mamani <janeth@ibm.com>
2021-08-03 12:40:11 -0300

    Projeto Indicadores de Banco enviado ao git

---
+++ IndicadoresBanco/IndicadoresBanco/src/main/java/com/ibm/IndicadoresBanco/IndicadoresBancoApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.IndicadoresBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicadoresBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicadoresBancoApplication.class, args);
+	}
+
+}
---
+++ IndicadoresBanco/IndicadoresBanco/src/test/java/com/ibm/IndicadoresBanco/IndicadoresBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.IndicadoresBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicadoresBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
